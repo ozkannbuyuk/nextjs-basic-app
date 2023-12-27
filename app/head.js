ca431593417fd1bac3624ac51404667049b6b1bf
ozkanbuyuk <ozkanbuyuk@icloud.com>
2023-12-27 17:36:48 +0300

    Next.js - Basic App

---
+++ app/head.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Head() {
+  return (
+    <>
+      <title>Next.js - Basic App</title>
+      <meta name="description" content="Next.js - Basic App" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
+
+export default Head;
